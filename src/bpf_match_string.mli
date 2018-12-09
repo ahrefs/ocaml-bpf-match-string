281db71ac0b2e4428bf01332a0cb3af2a58c3455
Raman Varabets <roman.vorobets@gmail.com>
2018-12-09 13:08:48 +0800

    update documentation in mli

--- src/bpf_match_string.mli
+++ src/bpf_match_string.mli
@@ -2,23 +2,33 @@ type t
 
 type code =
   | Skip of int
-    (** [Skip n] generates instructions to skip [n] chars or exit false if reaching EOS *)
+    (** [Skip n] skips [n] chars or
+       returns false if EOS would be reached *)
   | MatchString of string
-    (** [Match_string "hello"] tries to match current postion with "hello" or exit false *)
+    (** [MatchString s] matches current postion with string [s] or
+       returns false if there is no match *)
   | MatchChars of char list
-    (** [Match_chars l] will exit true if char at current position is one of the list [l]; does not advance position *)
+    (** [MatchChars l] matches current position with one of characters in [l] or
+        returns false if there is no match; does not advance the current position *)
   | MismatchChars of char list
-    (** [Mismatch_chars l] will exit false if char at current position is one of the list [l]; does not advance position *)
+    (** [MismatchChars l] ensures current position does not match one of characters in [l] or
+        returns false if there is a match; does not advance the current position *)
   | CheckEOS of bool
     (**
-       [Check_eos false] will exit false if current position is end of string
-       [Check_eos true] will exit false if current position is not end of string
+       [CheckEOS true] checks if current position is end of string, returns false on no EOS
+       [CheckEOS false] checks if current position is not end of string, returns false on EOS
     *)
   | SkipToChar of char
-    (** [Skip_to_char c] generates instructions to move just after the first occurence of c exit false if reaching EOS *)
+  (** [SkipToChar c] moves current position just after the first occurence of [c] or
+      returns false if EOS is reached in the process *)
   | And of code list list
+  (** [And [prog1; prog2; ...]] ensures all of [prog1, prog2, ...] match, returns false otherwise;
+      does not advance the current position *)
   | Or of code list list
+  (** [Or [prog1; prog2; ...]] ensures one of [prog1, prog2, ...] match, returns false otherwise;
+     DOES advance the current position *)
   | Not of code list
+  (** [Not prog] ensures prog does not match; returns false if it does. *)
 
 val string_of_code : code -> string
 
@@ -29,8 +39,8 @@ val assemble : t -> string
 (* example :
     assemble @@ make [
       Skip 4;
-      Skip_to_char '%';
-      Match_string "hello world";
-      Check_eos true;
+      SkipToChar '%';
+      MatchString "hello world";
+      CheckEOS true;
     ]
 *)
